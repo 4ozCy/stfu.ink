587cd5a1420bc5710b0c014fdcb0d759bef03ffb
Zcy. <157526994+4ozCy@users.noreply.github.com>
2025-08-22 12:37:34 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const favicon = require('serve-favicon');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(favicon(path.join(__dirname, 'stfu.png')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/dc', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dc.html'));
+});
+
+app.get('/:file', (req, res) => {
+  const fileName = req.params.file;
+  const filePath = path.join(__dirname, fileName);
+
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      const notFoundPath = path.join(__dirname, '404.html');
+      res.status(404).sendFile(notFoundPath);
+    }
+  });
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Error in server setup');
+  } else {
+    console.log(`Server listening on Port ${PORT}`);
+  }
+});
