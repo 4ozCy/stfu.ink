5091f7f0d6a253f83d2aeccd011a73e873ac4083
Zcy. <157526994+4ozCy@users.noreply.github.com>
2025-09-12 14:48:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,42 @@ const express = require('express');
 const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
 const favicon = require('serve-favicon');
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(favicon(path.join(__dirname, 'stfu.png')));
-app.use(express.static(__dirname));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/dc', (req, res) => res.sendFile(path.join(__dirname, 'dc.html')));
-app.get('/.well-known/discord', (req, res) => res.send('dh=6cfeb553898f125458c3bcdf4404c84a7a412fe9'));
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/dc', (req, res) => {
+res.sendFile(path.join(__dirname, 'dc.html'));
+});
+
+app.get('/.well-known/discord', (req, res) => {
+res.send('dh=6cfeb553898f125458c3bcdf4404c84a7a412fe9')
+});
+
 app.get('/:file', (req, res) => {
-  const fileName = req.params.file;
-  const filePath = path.join(__dirname, fileName);
-  res.sendFile(filePath, err => {
-    if (err) res.status(404).sendFile(path.join(__dirname, '404.html'));
-  });
+const fileName = req.params.file;
+const filePath = path.join(__dirname, fileName);
+
+res.sendFile(filePath, (err) => {
+if (err) {
+const notFoundPath = path.join(__dirname, '404.html');
+res.status(404).sendFile(notFoundPath);
+}
 });
-app.get('/q', async (req, res) => {
-  try {
-    const response = await fetch('https://zenquotes.io/api/random');
-    const data = await response.json();
-    res.json({ text: data[0].q, author: data[0].a });
-  } catch (err) {
-    res.status(500).json({ text: 'Failed to fetch quote.', author: '' });
-  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, (err) => {
+if (err) {
+console.error('Error in server setup');
+} else {
+console.log(Server listening on Port ${PORT});
+}
 });
